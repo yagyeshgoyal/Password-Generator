8553aa2e20865540de8c0290d45b2fb822410353
yagyeshgoyal <yagyeshgoyal123@gmail.com>
2024-06-28 21:08:46 +0530

    add css

--- script.js
+++ script.js
@@ -21,17 +21,22 @@ let passwordLength = 10;
 let checkCount = 1;
 handleSlider(); 
 // ste strength circle color to grey
+setIndicator("#ccc");
 
 //set passwordLength
 function handleSlider(){
     inputSlider.value = passwordLength;
     lengthDisplay.innerText = passwordLength;
     //or kuch bhi karna chahiye ?
+    const min = inputSlider.min;
+    const max = inputSlider.max;
+    inputSlider.style.backgroundSize = ((passwordLength-min)*100/(max-min)) + "% 100%";
 }
 
 function setIndicator(color){
     indicator.style.backgroundColor = color;
     //shadow
+    indicator.style.boxShadow = `0px 0px 12px 1px ${color}`;
 }
 
 function getRanInteger(min,max){
@@ -210,5 +215,5 @@ generateBtn.addEventListener('click', () =>{
     console.log("ui par password");
 
 
-    // calcStrength();
+    calcStrength();
 })
